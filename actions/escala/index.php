f0cce86859a49712cabda22a2e26060e4b97997a
Guilherme de Lima <guilhermeliima74@gmail.com>
2021-01-28 22:49:34 -0300

    update 1

---
+++ actions/escala/index.php
@@ -0,0 +1,37 @@
+<?php
+
+$daysweek = array(
+    'Sun',
+    'Mon',
+    'Tue',
+    'Wed',
+    'Thu',
+    'Fri',
+    'Sat'
+);
+
+$diasSemana = array(
+    'Domingo',
+    'Segunda',
+    'Terça',
+    'Quarta',
+    'Quinta',
+    'Sexta',
+    'Sabado'
+);
+
+$months = array(
+    1 => 'Janeiro',
+    2 => 'Fevereiro',
+    3 => 'Março',
+    4 => 'Abril',
+    5 => 'Maio',
+    6 => 'Junho',
+    7 => 'Julho',
+    8 => 'Agosto',
+    9 => 'setembro',
+    10 => 'Outubro',
+    11 => 'Novembro',
+    12 => 'Dezembro'
+);
+
